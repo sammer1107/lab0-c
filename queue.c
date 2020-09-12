d23a89f56773790d1aa48d6d1478e33a4f536aff
sammer1107 <qscesz871107@gmail.com>
2020-09-12 15:32:04 +0800

    Implement q_free
    need to Implement copy string in insert function ASAP.
    Otherwise, the freed location of the string will be wrong.

--- queue.c
+++ queue.c
@@ -22,8 +22,14 @@ queue_t *q_new()
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
-    /* TODO: How about freeing the list elements and the strings? */
-    /* Free queue structure */
+    list_ele_t *next;
+    while (q->head) {
+        next = q->head->next;
+        free(q->head->value);
+        free(q->head);
+        q->head = next;
+    }
+
     free(q);
 }
 
