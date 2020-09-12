2945f7627f64f6276ba537447f397cd8a625e83b
sammer1107 <qscesz871107@gmail.com>
2020-09-12 15:13:46 +0800

    Fix the problem where new queue allocation fails

--- queue.c
+++ queue.c
@@ -12,9 +12,10 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
-    q->size = 0;
+    if (q) {
+        q->head = NULL;
+        q->size = 0;
+    }
     return q;
 }
 
